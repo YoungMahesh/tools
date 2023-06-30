ada4d40185cd962248bc19e1a3c760dbfe9ce992
Mahesh Ghamand <maheshghamand@gmail.com>
2023-06-30 13:37:45 +0000

    merged EVMWallets & SectionBox components

--- src/components/evm/EVMWallets.tsx
+++ src/components/evm/EVMWallets.tsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
-import SectionBox from "./SectionBox";
 import { Wallet } from "ethers";
+import KeyBox from "../common/KeyBox";
 
 export default function EVMWallets() {
   const [pubkey, setPubkey] = useState("");
   const [secretKey, setSecretKey] = useState("");
+  const [privateK, setPrivateK] = useState("");
+  const [publicK, setPublicK] = useState("");
+
+  const derivePublicKey = () => {
+    try {
+      const wallet = new Wallet(privateK);
+      setPublicK(wallet.address);
+    } catch (err) {
+      alert("Invalid private key");
+    }
+  };
 
   const generateKeyPair = () => {
     const keypair = Wallet.createRandom();
@@ -12,17 +23,42 @@ export default function EVMWallets() {
     setPubkey(keypair.address);
     setSecretKey(keypair.privateKey);
   };
+
   return (
-    <SectionBox
-      generateFun={generateKeyPair}
-      arr1={
-        pubkey.length
-          ? [
-              { title1: "Public Key: ", value1: pubkey },
-              { title1: "Private Key: ", value1: secretKey },
-            ]
-          : []
-      }
-    />
+    <section className="grid w-screen place-content-center">
+      <div className="m-2 rounded border p-1">
+        <div className="mb-5 flex justify-center">
+          <button className="btn-primary btn" onClick={generateKeyPair}>
+            Generate New
+          </button>
+        </div>
+        {pubkey.length ? (
+          <div className="flex flex-wrap">
+            <KeyBox title1="Public Key: " value1={pubkey} />
+            <KeyBox title1="Private Key: " value1={secretKey} />
+          </div>
+        ) : null}
+      </div>
+
+      <div className="m-2 rounded border p-1">
+        <div className="mb-5 flex flex-wrap items-center justify-center">
+          <input
+            type="text"
+            placeholder="Private Key"
+            className="input-bordered input-primary input w-full"
+            value={privateK}
+            onChange={(e) => setPrivateK(e.target.value)}
+          />
+          <button className="btn-primary btn m-1" onClick={derivePublicKey}>
+            Get Public Key
+          </button>
+        </div>
+        <div className="flex ">
+          {publicK.length ? (
+            <KeyBox title1="Public Key: " value1={publicK} />
+          ) : null}
+        </div>
+      </div>
+    </section>
   );
 }
